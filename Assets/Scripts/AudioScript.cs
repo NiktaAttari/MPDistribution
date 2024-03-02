302c6b060472b2e381e8475f67a74aa42d4608f8
Nikta <niktaat2000@gmail.com>
2024-03-01 17:04:10 -0800

    incineration facility

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioSource audioSource1;
+    public AudioSource audioSource2;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSource1.Play();
+        audioSource2.Play();
+    }
+}
